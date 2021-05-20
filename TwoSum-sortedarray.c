01bdc77944f904cc2aa4a08b29316fd9f3975c39
Eshan Naik <57188348+eshannaik@users.noreply.github.com>
2021-05-20 14:15:02 +0530

    Update TwoSum-sortedarray.c

--- TwoSum-sortedarray.c
+++ TwoSum-sortedarray.c
@@ -1,23 +1,20 @@
 //https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
 
 int* twoSum(int* numbers, int numbersSize, int target, int* returnSize){
-    int sum=0;
     int *r = malloc(2*(sizeof(int)));
     *returnSize = 2;
         
-    for (int i=0;i<numbersSize;i++)
+    for (int i=0;i<numbersSize-1;i++)
     {
         for(int j=(i+1);j<numbersSize;j++)
         {
-            sum = numbers[i]+numbers[j];
-            if(sum==target)
+            if(numbers[i]+numbers[j]==target)
             {
                 r[0]=i+1;
                 r[1]=j+1;
                 return r;
             }
         }
-        sum=0;
     }
     return 0;
 }
