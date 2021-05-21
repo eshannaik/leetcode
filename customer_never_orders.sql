ab317e07e6beb4df04bf081f03acde570988922b
Eshan Naik <57188348+eshannaik@users.noreply.github.com>
2021-05-21 17:28:53 +0530

    Update customer_never_orders.sql

--- customer_never_orders.sql
+++ customer_never_orders.sql
@@ -1,4 +1,4 @@
-//https://leetcode.com/problems/customers-who-never-order/
+-- https://leetcode.com/problems/customers-who-never-order/
 
 SELECT Name as Customers from Customers WHERE Id NOT IN (
     SELECT CustomerId from Orders
