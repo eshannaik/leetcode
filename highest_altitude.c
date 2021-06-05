31fe76bed32b1508be6c07e9ad218ef7a7a0d444
Eshan Naik <57188348+eshannaik@users.noreply.github.com>
2021-06-05 16:15:09 +0530

    Create highest_altitude.c

---
+++ highest_altitude.c
@@ -0,0 +1,20 @@
+//https://leetcode.com/problems/find-the-highest-altitude/submissions/
+
+
+int largestAltitude(int* gain, int gainSize){
+    int diff = 0,max=0;
+    int *temp = malloc((gainSize+1) *(sizeof(int)));
+    temp[0]=0;
+    temp[1]=gain[0];
+    for (int i=0;i<(gainSize-1);i++)
+    {
+        temp[i+2] = temp[i+1]+gain[i+1];
+        printf("%d = %d + %d\n",temp[i+2],temp[i],gain[i+1]);
+    }
+    for(int i=0;i<(gainSize+1);i++)
+    {
+        if(temp[i]>max)
+            max=temp[i];
+    }
+    return max;
+}
