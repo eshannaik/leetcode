ed86294c476b6849903949f7b83c33caf0c360e6
Eshan Naik <57188348+eshannaik@users.noreply.github.com>
2021-06-12 15:48:36 +0530

    Create time.cpp

---
+++ time.cpp
@@ -0,0 +1,30 @@
+//https://leetcode.com/problems/latest-time-by-replacing-hidden-digits/submissions/
+
+class Solution {
+public:
+    string maximumTime(string time) {
+        int c=0;
+        if(time[0]=='?')
+        {
+            printf("%c",time[1]);
+            if(time[1]<'4' || time[1]=='?')
+                time[0]='2';
+            else if(time[1]<'4')
+                time[0]='2';
+            else
+                time[0]='1';
+        }
+        
+        if(time[1]=='?' && time[0]=='2')
+            time[1]='3';
+        else if(time[1]=='?')
+            time[1]='9';
+        
+        if(time[3]=='?')
+            time[3]='5';
+        
+        if(time[4]=='?')
+            time[4]='9';
+        return time;
+    }
+};
