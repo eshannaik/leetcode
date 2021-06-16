4c9a667a5167bfc77ab3907a144e62093ee32e19
Eshan Naik <57188348+eshannaik@users.noreply.github.com>
2021-06-16 16:39:17 +0530

    Update longer-contiguous-segments-of-ones-than-zeros.cpp

--- longer-contiguous-segments-of-ones-than-zeros.cpp
+++ longer-contiguous-segments-of-ones-than-zeros.cpp
@@ -22,3 +22,19 @@ int o=0,z=0,maxo=0,maxz=0;
             return true;
         else
             return false;
+
+// https://leetcode.com/problems/max-consecutive-ones/
+
+int max=0,c=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==1)
+            {
+                c++;
+                if(c>max)
+                    max=c;
+            }
+            else
+                c=0;
+        }
+        return max;
