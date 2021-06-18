3b018ad17b14a0170f7a2ba32ce6fcd51f89b509
Eshan Naik <57188348+eshannaik@users.noreply.github.com>
2021-06-18 13:28:41 +0530

    Create Path_sum_tree.cpp

---
+++ Path_sum_tree.cpp
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/path-sum/
+
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if (root == NULL)
+            return false;
+        if(!root->left && !root->right && root->val==targetSum)
+            return true;
+        return hasPathSum(root->left,targetSum-root->val) || hasPathSum(root->right,targetSum-root->val);
+    }
+};
