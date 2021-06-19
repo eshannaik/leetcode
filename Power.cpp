16df46d2c9bc903a2677d3b659042417b0c62eb9
Eshan Naik <57188348+eshannaik@users.noreply.github.com>
2021-06-19 20:23:37 +0530

    Update Power.cpp

--- Power.cpp
+++ Power.cpp
@@ -20,3 +20,26 @@ double p=1,sign=0;
         if(sign==1)
             p=1/p;
         return p;
+
+// https://leetcode.com/problems/power-of-two/submissions/
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        // long x=2;
+        // if(n==1)
+        //     return 1;
+        // if(n==2)
+        //     return 1;
+        // while(x>0)
+        // {
+        //     x>>=1;
+        //     if(x==n)
+        //         return true;
+        //     if(x>INT_MAX)
+        //         break;
+        // }
+        // return false;
+        return (n>0 && 1073741824%n==0);
+    }
+};
