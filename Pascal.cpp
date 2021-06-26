8a38b40d829d9d755920ad91d84a90547841f5a2
Eshan Naik <57188348+eshannaik@users.noreply.github.com>
2021-06-26 19:33:54 +0530

    Update Pascal.cpp

--- Pascal.cpp
+++ Pascal.cpp
@@ -16,3 +16,31 @@ public:
         return v;
     }
 };
+
+// https://leetcode.com/problems/pascals-triangle-ii/submissions/
+
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector <vector<int>> v;
+        vector<int>res(rowIndex+1,-1);
+        if(rowIndex==0)
+        {
+            res[0]=1;
+            return res;
+        }
+        res[0]=1;
+        res[1]=1;
+        if(rowIndex==1)
+            return res;
+        for(int i=2;i<=rowIndex;i++)
+        {
+            res[i]=1;
+            for(int j=i-1;j>0;j--)
+            {
+                res[j]=res[j]+res[j-1];
+            }
+        }
+        return res;
+    }
+};
