fb31632132b47d5b3292fa6176aeb25319564334
Eshan Naik <57188348+eshannaik@users.noreply.github.com>
2021-08-18 16:17:53 +0530

    Update binary_bfs.cpp

--- binary_bfs.cpp
+++ binary_bfs.cpp
@@ -29,3 +29,37 @@ public:
         return result;
     }
 };
+
+// PART 2
+// https://leetcode.com/problems/binary-tree-level-order-traversal-ii/submissions/
+
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        if(root==NULL)
+            return {};
+        TreeNode* temp;
+        queue <TreeNode*> res;
+        res.push(root);
+        vector <int> v;
+        vector<vector<int>> result;
+        int l;
+        while(!res.empty()){
+            l = res.size();
+            for(int i=0;i<l;i++){
+                temp = res.front();
+                res.pop();
+                v.push_back(temp->val);
+                
+                if(temp->left)
+                    res.push(temp->left);
+                if(temp->right)
+                    res.push(temp->right);
+            }
+            result.push_back(v);
+            v.clear();
+        }
+        reverse(result.begin(),result.end());
+        return result;
+    }
+};
