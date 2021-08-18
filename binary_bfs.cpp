de3f58242caaaf078117bff3e01d25f7f5ee2ccb
Eshan Naik <57188348+eshannaik@users.noreply.github.com>
2021-08-18 16:03:08 +0530

    Create binary_bfs.cpp

---
+++ binary_bfs.cpp
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/cousins-in-binary-tree/submissions/
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        if(root==NULL)
+            return {};
+        TreeNode* temp;
+        queue <TreeNode*> res;
+        res.push(root);
+        vector <int> v;
+        vector<vector<int>> result;
+        int l;
+        while(!res.empty()){
+            l = res.size();
+            for(int i=0;i<l;i++){
+                temp = res.front();
+                res.pop();
+                v.push_back(temp->val);
+                
+                if(temp->left)
+                    res.push(temp->left);
+                if(temp->right)
+                    res.push(temp->right);
+            }
+            result.push_back(v);
+            v.clear();
+        }
+        return result;
+    }
+};
