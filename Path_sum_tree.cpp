419429e25ff701f7c6d2e05c5b8f9ffe447ff845
Eshan Naik <57188348+eshannaik@users.noreply.github.com>
2021-08-23 16:17:05 +0530

    Update Path_sum_tree.cpp

--- Path_sum_tree.cpp
+++ Path_sum_tree.cpp
@@ -10,3 +10,33 @@ public:
         return hasPathSum(root->left,targetSum-root->val) || hasPathSum(root->right,targetSum-root->val);
     }
 };
+
+// https://leetcode.com/problems/path-sum-ii/
+
+class Solution {
+public:
+    void inorder(TreeNode* root,vector <int> &v){
+        if(root==NULL)
+            return;
+        inorder(root->left,v);
+        v.push_back(root->val);
+        inorder(root->right,v);
+    }
+    bool findTarget(TreeNode* root, int k) {
+        vector <int> v;
+        inorder(root,v);
+        sort(v.begin(),v.end());
+        int l=0,r=v.size()-1;
+        while(l<r)
+        {
+            int sum = v[l]+v[r];
+            if(sum<k)
+                l++;
+            if(sum>k)
+                r--;
+            if(sum==k)
+                return true;
+        }
+        return false;
+    }
+};
