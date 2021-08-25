da62f2a43f1955131ad2bd6b2aa446b4f610f783
Eshan Naik <57188348+eshannaik@users.noreply.github.com>
2021-08-25 13:46:17 +0530

    Create counting_bits.cpp

---
+++ counting_bits.cpp
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/counting-bits/
+
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector <int> res(n+1,0);
+        res[0]=0;
+        res[1]=1;
+        for(int i=2;i<=n;i++)
+        {
+            if(i%2==0)
+                res[i]=res[i/2];
+            else
+                res[i]=res[i-1]+1;
+        }
+        return res;
+    }
+};
