0bceb48a18bbe12925e5d653c347fa7d212f2539
Eshan Naik <57188348+eshannaik@users.noreply.github.com>
2021-08-25 13:49:05 +0530

    Update counting_bits.cpp

--- counting_bits.cpp
+++ counting_bits.cpp
@@ -5,6 +5,8 @@ public:
     vector<int> countBits(int n) {
         vector <int> res(n+1,0);
         res[0]=0;
+        if(n==0)
+            return res;
         res[1]=1;
         for(int i=2;i<=n;i++)
         {
