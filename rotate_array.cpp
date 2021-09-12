755b057c3411d43946d2a827b42664f82a375739
Eshan Naik <57188348+eshannaik@users.noreply.github.com>
2021-09-12 09:38:54 +0530

    Create rotate_array.cpp

---
+++ rotate_array.cpp
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/rotate-array/submissions/(
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n=nums.size();
+        
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k%n);
+        reverse(nums.begin()+k%n,nums.end());
+    }
+};
